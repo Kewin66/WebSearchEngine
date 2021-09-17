32627bb9b5db493abd6cc054378ddc2f82d4e51a
Kewin V <kewindev96@gmail.com>
2021-09-17 14:52:04 -0400

    Streak

---
+++ Assignment1/TandonSpider/TandonSpider/spiders/RobotResponse.py
@@ -0,0 +1,13 @@
+import os
+result = os.popen("curl https://fortune.com/robots.txt").read()
+result_data_set = {"Disallowed": [], "Allowed": []}
+
+for line in result.split("\n"):
+    if line.startswith('Allow'):    # this is for allowed url
+        result_data_set["Allowed"].append(line.split(': ')[1].split(
+            ' ')[0])    # to neglect the comments or other junk info
+    elif line.startswith('Disallow'):    # this is for disallowed url
+        result_data_set["Disallowed"].append(line.split(': ')[1].split(
+            ' ')[0])    # to neglect the comments or other junk info
+
+print(result_data_set)
